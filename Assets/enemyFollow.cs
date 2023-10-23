ce48d702bcbe7a3a1c5ca124610492b9cf2d364b
Joe <joe.cordero@udla.edu.ec>
2023-10-23 14:56:58 -0500

    Enemy script for following the player

--- Assets/enemyFollow.cs
+++ Assets/enemyFollow.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class enemyFollow : MonoBehaviour
 {
+    public NavMeshAgent myEnemy;
+    public Transform myPlayer;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,7 @@ public class enemyFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        myEnemy.SetDestination(myPlayer.position);
         
     }
 }
